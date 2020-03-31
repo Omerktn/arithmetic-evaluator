ff0682d7ce1be129954a95e7994ac9ca8cef017c
Ömer Kurttekin <o.kurttekin@hotmail.com>
2020-03-31 19:56:07 +0300

    Update arthm.c
    special functions, constants added

--- arthm.c
+++ arthm.c
@@ -3,17 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #define MAX_STACK 2056
-#define MAX_CHAR_IN 2056
-#define PI 3.14159265358979323846
-#define EU 2.71828182845904523536
-
-#define MIN(X, Y) (((X) > (Y)) ? (Y) : (X))
+#define MAX_CHAR_IN 4096
+#define PI_CONST 3.14159265358979323846
+#define EU_CONST 2.71828182845904523536
 
 #define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_GREEN "\x1b[32m"
-#define ANSI_COLOR_YELLOW "\x1b[01;33m" //\x1b[33m"
+#define ANSI_COLOR_YELLOW "\x1b[01;33m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
 typedef struct {
@@ -81,12 +79,12 @@ void printStack(STACK *s) {
 
 /* Return 1 if ch1 has higher priority */
 int isPrior(char ch1, char ch2) {
-  char chList[6] = {'(', '^', '*', '/', '+', '-'};
-  char chPrio[6] = {4, 3, 2, 2, 1, 1};
+  char chList[7] = {'(', '|', '^', '*', '/', '+', '-'};
+  char chPrio[7] = {5, 4, 3, 2, 2, 1, 1};
   short prv1 = -1, prv2 = -1;
   int i = 0;
 
-  while (i < 6) {
+  while (i < 7) {
     if (ch1 == chList[i]) {
       prv1 = chPrio[i];
     }
@@ -107,9 +105,10 @@ int isPrior(char ch1, char ch2) {
   return (prv1 >= prv2);
 }
 
+/* Return 1 if the string contains the char  */
 int isContain(char ch, char *in) {
-  int i=0, ilen=strlen(in);
-  while(i < ilen) {
+  int i = 0, ilen = strlen(in);
+  while (i < ilen) {
     if (in[i] == ch) {
       return 1;
     }
@@ -118,44 +117,142 @@ int isContain(char ch, char *in) {
   return 0;
 }
 
-/* Takes a string, returns an array of strings, e.g. "260+(4)" -> {"260","+","(","4",")"} */
+/* Free all the elements of string array */
+void deleteExpression(char **strlist, int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    free(strlist[i]);
+  }
+  free(strlist);
+}
+
+/* Return math function number, returns -1 if no function name matched */
+int getFunctionNum(char *fname) {
+  char *funcNames[8] = {"sin",    "cos",    "tan",    "cot",
+                        "arctan", "arcsin", "arccos", "sqrt"};
+  int i = 0;
+  // Check for functions
+  while (i < 8) {
+    if (!strcasecmp(fname, funcNames[i])) {
+      return i;
+    }
+    i++;
+  }
+
+  return -1;
+}
+
+int getConstant(char *fname, float *constVal, float formerAns) {
+  char *constNames[3] = {"pi", "e", "m"};
+  float constVals[3] = {PI_CONST, EU_CONST, formerAns};
+  int i = 0;
+  // Check for constants
+  while (i < 3) {
+    if (!strcasecmp(fname, constNames[i])) {
+      *constVal = constVals[i];
+      return 1;
+    }
+    i++;
+  }
+
+  // Not a constant
+  return 0;
+}
+
+/* Takes a string, returns an array of strings,
+ * e.g. "260+(4)" -> {"260","+","(","4",")"} */
 char **strToArray(char *in, int slen, int *ilen, float formerAns) {
-  char **str = (char **)malloc(sizeof(char *) * slen);
+  char **str = (char **)malloc(sizeof(char *) * slen * 2);
   char tmp[MAX_CHAR_IN];
-  int i = 0, arrIndex = 0, t = 0;
+  float constVal;
+  int i = 0, arrIndex = 0, t = 0, funcNum,
+      textNumState = 0; // textNumState 1:text 2:num 0:don't care
 
   for (i = 0; i < slen; i++) {
     if (isdigit(in[i]) || in[i] == '.') {
-      tmp[t++] = in[i];
-    }
-    else if (isContain(tolower(in[i]), "mpe")){
-      if (t == 0) {
-        switch(tolower(in[i])) {
-        case 'm':
-          sprintf(tmp, "%f", formerAns);
-          break;
-        case 'e':
-          sprintf(tmp, "%f", EU);
-          break;
-        case 'p':
-          sprintf(tmp, "%f", PI);
-          break;
+
+      if (textNumState == 1) {
+        // Switched from text state
+        tmp[t++] = '\0';
+        str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
+
+        funcNum = getFunctionNum(tmp);
+        if (funcNum >= 0) {
+          // Operand is a function
+          sprintf(tmp, "%d", funcNum);
+          strcpy(str[arrIndex++], tmp);
+
+          // Put special math function indicator
+          str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
+          strcpy(str[arrIndex++], "|");
+
+        } else if (getConstant(tmp, &constVal, formerAns)) {
+          // Operand is a constant
+          sprintf(tmp, "%f", constVal);
+          strcpy(str[arrIndex++], tmp);
+
+        } else {
+          // Neither constant nor function
+          printf(ANSI_COLOR_RED);
+          printf("Unknown function or constant: ");
+          printf(ANSI_COLOR_RESET);
+          printf("%s\n", tmp);
         }
-        str[arrIndex] = (char *)malloc(sizeof(char) * (strlen(tmp)+1));
+
+        t = 0;
+        tmp[t++] = in[i];
+      } else {
+        // Number state keeps
+        tmp[t++] = in[i];
+      }
+      textNumState = 2;
+
+    } else if (isalpha(in[i])) {
+
+      if (textNumState == 2) {
+        // Switched from num state
+        tmp[t++] = '\0';
+        str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
         strcpy(str[arrIndex++], tmp);
         t = 0;
+
+        tmp[t++] = in[i];
+      } else {
+        // Text state keeps
+        tmp[t++] = in[i];
       }
-      else {
-        printf(ANSI_COLOR_RED);
-        printf("Syntax error near ");
-        printf(ANSI_COLOR_RESET);
-        printf("%c\n",in[i]);
-      }
-    }
-    else {
-      if (t) {
+      textNumState = 1;
+    } else {
+
+      if (textNumState == 1) {
+        //  Transform text to function number
         tmp[t++] = '\0';
-        str[arrIndex] = (char *)malloc(sizeof(char) * (t + 1));
+        str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
+        funcNum = getFunctionNum(tmp);
+        if (funcNum >= 0) {
+          // operand is a function
+          sprintf(tmp, "%d", funcNum);
+          strcpy(str[arrIndex++], tmp);
+
+          // Put special math function indicator
+          str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
+          strcpy(str[arrIndex++], "|");
+
+        } else if (getConstant(tmp, &constVal, formerAns)) {
+          // Operand is a constant
+          sprintf(tmp, "%f", constVal);
+          strcpy(str[arrIndex++], tmp);
+
+        } else {
+          // Neither constant nor function
+          printf(ANSI_COLOR_RED);
+          printf("Unknown function or constant: ");
+          printf(ANSI_COLOR_RESET);
+          printf("%s\n", tmp);
+        }
+      } else if (t) {
+        tmp[t++] = '\0';
+        str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
         strcpy(str[arrIndex++], tmp);
       }
 
@@ -164,16 +261,36 @@ char **strToArray(char *in, int slen, int *ilen, float formerAns) {
       str[arrIndex][1] = '\0';
       arrIndex++;
       t = 0;
+      textNumState = 0;
     }
   }
   if (t) {
     tmp[t] = '\0';
     str[arrIndex] = (char *)malloc(sizeof(char) * (t + 1));
-    strcpy(str[arrIndex++], tmp);
+
+    funcNum = getFunctionNum(tmp);
+    if (funcNum >= 0) {
+      // operand is a function
+      sprintf(tmp, "%d", funcNum);
+      strcpy(str[arrIndex++], tmp);
+
+      // Put special math function indicator
+      str[arrIndex] = (char *)malloc(sizeof(char) * (t + 16));
+      strcpy(str[arrIndex++], "|");
+
+    } else if (getConstant(tmp, &constVal, formerAns)) {
+      // Operand is a constant
+      sprintf(tmp, "%f", constVal);
+      strcpy(str[arrIndex++], tmp);
+
+    } else {
+      // Neither constant nor function
+      strcpy(str[arrIndex++], tmp);
+    }
   }
 
   *ilen = arrIndex;
-  str = (char **)realloc(str, sizeof(char *) * arrIndex);
+  // str = (char **)realloc(str, sizeof(char *) * arrIndex);
   return str;
 }
 
@@ -182,14 +299,14 @@ char **infixToPostfix(char **in, int ilen, int *plen) {
   STACK *s = (STACK *)malloc(sizeof(STACK));
   int i, p = 0;
   float tmp;
-  char *op;
+  char op[MAX_CHAR_IN];
   initStack(s);
 
   for (i = 0; i < ilen; i++) {
-    op = in[i];
+    strcpy(op, in[i]);
 
     if (isdigit(op[0])) {
-      // An operand
+      // A digit
       postf[p] = (char *)malloc(sizeof(char) * strlen(op));
       strcpy(postf[p++], op);
 
@@ -197,7 +314,7 @@ char **infixToPostfix(char **in, int ilen, int *plen) {
       peek(s, &tmp);
       while (!isEmpty(s) && (char)tmp != '(') {
         pop(s, &tmp);
-        postf[p] = (char *)malloc(sizeof(char) * 3);
+        postf[p] = (char *)malloc(sizeof(char) * 16);
         postf[p][0] = tmp;
         postf[p++][1] = '\0';
         peek(s, &tmp);
@@ -207,7 +324,7 @@ char **infixToPostfix(char **in, int ilen, int *plen) {
       peek(s, &tmp);
       while (isPrior((char)tmp, op[0]) && !isEmpty(s) && (char)tmp != '(') {
         pop(s, &tmp);
-        postf[p] = (char *)malloc(sizeof(char) * 3);
+        postf[p] = (char *)malloc(sizeof(char) * 16);
         postf[p][0] = tmp;
         postf[p++][1] = '\0';
         peek(s, &tmp);
@@ -220,15 +337,58 @@ char **infixToPostfix(char **in, int ilen, int *plen) {
 
   while (!isEmpty(s)) {
     pop(s, &tmp);
-    postf[p] = (char *)malloc(sizeof(char) * 3);
+    postf[p] = (char *)malloc(sizeof(char) * 16);
     postf[p][0] = tmp;
     postf[p++][1] = '\0';
   }
 
   free(s);
   *plen = p;
-  postf = (char **)realloc(postf, sizeof(char) * p);
   return postf;
+}
+
+float mathFunction(int func_num, float input) {
+  float ans;
+
+  switch (func_num) {
+  case 0:
+    // Sinus
+    ans = sin(input);
+    break;
+  case 1:
+    // Cosinus
+    ans = cos(input);
+    break;
+  case 2:
+    // Tangent
+    ans = tan(input);
+    break;
+  case 3:
+    // Cotangent
+    ans = cos(input) / sin(input);
+    break;
+  case 4:
+    // Arc-tangent
+    ans = atan(input);
+    break;
+  case 5:
+    // Arc-sinus
+    ans = asin(input);
+    break;
+  case 6:
+    // Arc-cosinus
+    ans = acos(input);
+    break;
+  case 7:
+    // Square root
+    ans = sqrt(input);
+    break;
+  default:
+    ans = 0;
+    break;
+  }
+
+  return ans;
 }
 
 float perform(char op, float op1, float op2) {
@@ -250,6 +410,9 @@ float perform(char op, float op1, float op2) {
   case '^':
     ans = pow(op1, op2);
     break;
+  case '|':
+    ans = mathFunction(op1, op2);
+    break;
   default:
     ans = 0;
     break;
@@ -257,6 +420,7 @@ float perform(char op, float op1, float op2) {
   return ans;
 }
 
+/* Evaluate the operations, with using stack */
 float evalPostfix(char **postf, int plen) {
   STACK *s = (STACK *)malloc(sizeof(STACK));
   int i;
@@ -271,7 +435,6 @@ float evalPostfix(char **postf, int plen) {
       push(s, perform(postf[i][0], op1, op2));
     }
   }
-
   pop(s, &op1);
   free(s);
   return op1;
@@ -289,7 +452,7 @@ char *preProcess(char *infix) {
 
   while (infix[i] != '\0') {
     // Put zero before negative leftsided numbers
-    if (i > 0 && infix[i] == '-' && (infix[i-1] == '(')) {
+    if (i > 0 && infix[i] == '-' && (infix[i - 1] == '(')) {
       clean[c++] = '0';
     }
     // Aceppts all chars but space
@@ -336,11 +499,44 @@ int validCheck(char *in, int ilen) {
   return 1;
 }
 
+/* Evaluates and returns the answer. status will be set to -1 if any error
+ * occurs */
+float evalExpression(char *exp, int *status, float formerAns) {
+  char **postf, **infix;
+  int i, ilen, plen;
+  float ans;
+  *status = 1;
+  // Preprocess
+  strcpy(exp, preProcess(exp));
+  // Validation check
+  if (!validCheck(exp, strlen(exp))) {
+    *status = -1;
+    return 0;
+  }
+
+  infix = strToArray(exp, strlen(exp), &ilen, formerAns);
+  /*for (i = 0; i < ilen; i++) {
+    printf("%s, ", infix[i]);
+  }
+  printf("\n");*/
+
+  postf = infixToPostfix(infix, ilen, &plen);
+  /*
+  for (i=0; i<plen; i++) {
+    printf("len:%d :: ", (int)strlen(postf[i]));
+    printf("%s, \n", postf[i]);
+  }
+  printf("\n");*/
+  ans = evalPostfix(postf, plen);
+  deleteExpression(infix, ilen);
+  deleteExpression(postf, plen);
+  return ans;
+}
+
 int main() {
-  char in[MAX_CHAR_IN], **postf;
-  char **str;
-  int i, plen, ilen;
-  float ans, formerAns=0;
+  char in[MAX_CHAR_IN];
+  int status;
+  float ans, formerAns = 0;
   printf(ANSI_COLOR_RESET);
   printf("[Arithmetic Expression Evaluation]\n<Type 'q' to quit.>\n");
 
@@ -350,26 +546,19 @@ int main() {
     fgets(in, MAX_CHAR_IN, stdin);
     in[strlen(in) - 1] = '\0';
 
-    // Preprocess
-    strcpy(in, preProcess(in));
-    // Validation check
-    if (!validCheck(in, strlen(in))) {
-      continue;
+    ans = evalExpression(in, &status, formerAns);
+
+    if (status > 0) {
+      printf("Ans:>");
+      printf(ANSI_COLOR_YELLOW);
+
+      if (floor(ans) == (ans)) {
+        printf("%d\n", (int)ans);
+      } else {
+        printf("%f\n", ans);
+      }
+      formerAns = ans;
     }
-
-    str = strToArray(in, strlen(in), &ilen, formerAns);
-    postf = infixToPostfix(str, ilen, &plen);
-
-    printf("Ans:>");
-    printf(ANSI_COLOR_YELLOW);
-
-    ans = evalPostfix(postf, plen);
-    if (floor(ans) == (ans)) {
-      printf("%d\n", (int)ans);
-    } else {
-      printf("%f\n", ans);
-    }
-    formerAns = ans;
   } while (in[0] != 'q');
 
   return 0;
